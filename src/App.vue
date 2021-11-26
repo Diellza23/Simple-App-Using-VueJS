a2c2b4529acdf50a7e7bb3fec1d7bda71d2fbc51
Diellza23 <dk49556@ubt-uni.net>
2021-11-26 15:47:09 +0100

    First steps to a simple vuejs app

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+
+<div class="container">
+<component
+:is="screens[position]"
+/>
+
+</div>
+
+
+</template>
+
+<script>
+import appInitial from "./components/initial.vue";
+import appConfirm from "./components/confirm.vue";
+import appResults from "./components/results.vue";
+
+export default {
+  components: {
+    appInitial,
+    appConfirm,
+    appResults
+  },
+  data(){
+    return{
+      screens:['appInitial','appConfirm','appResults'],
+      position:0
+    }
+  }
+};
+</script>
+
+<style>
+@import "./assets/style.css";
+</style>
